e758c63677add91e9d38316ecbfff361e8dabe65
João Victor M <145566469+JVcT0r@users.noreply.github.com>
2025-06-03 10:59:21 -0300

    state

---
+++ Atv1/Assets/Scripts/State/StateEnemy.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StateEnemy : MonoBehaviour
+{
+    public StatePlayer StatePlayer;
+
+    private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // animator.SetFloat(Vector2.Distance())
+    }
+}
