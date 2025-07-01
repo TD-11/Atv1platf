66342664e8bf59bd90cd35a56de628dd954673dd
João Victor M <145566469+JVcT0r@users.noreply.github.com>
2025-07-01 08:18:35 -0300

    scriptable objects

---
+++ Atv1/Assets/Scripts/ScriptableObjects/EnemyRuntimeController.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class EnemyRuntimeController : MonoBehaviour
+{
+
+    public EnemySO enemyData;
+
+    public string EnemyName => enemyData.EnemyName;
+    public int HP=>enemyData.HP;
+    public int Damage=>enemyData.Damage;
+    public float Speed=>enemyData.Speed;
+    public Sprite EnemySprite=>enemyData.EnemySprite;
+    public Animator animator=>enemyData.animator;
+
+    private void OnValidate()
+    {
+        if (!enemyData) return;
+        GetComponent<SpriteRenderer>().sprite = EnemySprite;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
