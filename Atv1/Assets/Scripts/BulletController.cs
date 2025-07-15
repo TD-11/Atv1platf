15a6faf213d6699d49b43d8495efc333eab0ed2b
João Victor M. <145566469+JVcT0r@users.noreply.github.com>
2025-07-15 09:55:36 -0300

    objPool

---
+++ Atv1/Assets/Scripts/BulletController.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public Action<BulletController> OnDestroyBullet;
+    public float speed;
+    public Vector2 direction;
+
+    private void OnEnable()
+    {
+        Invoke("DestroyBullet", 2f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void DestroyBullet()
+    {
+        OnDestroyBullet?.Invoke(this);
+    }
+}
--- Atv1/Assets/Scripts/Sistemas/CircleController.cs
+++ Atv1/Assets/Scripts/Sistemas/CircleController.cs
@@ -1,10 +1,17 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Pool;
 using Random = UnityEngine.Random;
 
 public class CircleController : MonoBehaviour
 {
+   
+   public ObjectPool<BulletController> bulletPool;
+   public BulletController bulletPrefab;
    public SpriteRenderer _spriteRenderer;
+   
+   public float speed = 5f;
    
    [Header("Listening on...")]
    public VoidEventChannel circleColorEvent;
@@ -13,6 +20,44 @@ public class CircleController : MonoBehaviour
    private void Awake()
    {
       _spriteRenderer = GetComponent<SpriteRenderer>();
+   }
+
+   private void Start()
+   {
+      bulletPool = new ObjectPool<BulletController>(
+         CreateBullet,
+         GetBullet,
+         ReleaseBullet,
+         DestroyBullet,
+         false,
+         10,
+         20);
+   }
+
+   private void DestroyBullet(BulletController obj)
+   {
+      obj.OnDestroyBullet -= bulletPool.Release;
+      Destroy(obj.gameObject);
+   }
+
+   private void ReleaseBullet(BulletController obj)
+   {
+      obj.OnDestroyBullet -= bulletPool.Release;
+      obj.gameObject.SetActive(false);
+   }
+
+   private void GetBullet(BulletController obj)
+   {
+      obj.gameObject.SetActive(true);
+      obj.transform.position = transform.position;
+      obj.OnDestroyBullet += bulletPool.Release;
+   }
+
+   private BulletController CreateBullet()
+   {
+      BulletController bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+      bullet.OnDestroyBullet += bulletPool.Release;
+      return bullet;
    }
 
    private void OnEnable()
@@ -36,5 +81,37 @@ public class CircleController : MonoBehaviour
    {
       _spriteRenderer.color = corEspecifica;
    }
+
+   public void Update()
+   {
+      if (Keyboard.current.spaceKey.wasPressedThisFrame)
+      {
+         bulletPool.Get();
+         //Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+      }
+
+      if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+      {
+         transform.position += Vector3.left * (Time.deltaTime * speed);
+      }
+
+      if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+      {
+         transform.position += Vector3.right * (Time.deltaTime * speed);
+      }
+
+      if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+      {
+         transform.position += Vector3.up * (Time.deltaTime * speed);
+      }
+
+      if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+      {
+         transform.position += Vector3.down * (Time.deltaTime * speed);
+      }
+   }
+
+
+   
    
 }
